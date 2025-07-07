0cc4cade26f103d570e31e29c8dba328d87323ee
sahithdaddla <daddlasahith>
2025-07-07 12:33:52 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3089;
 
 // Database configuration
 const pool = new Pool({
